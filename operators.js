866b58903559866077c6b1956a5780397b8a57da
nancy-2414 <nancyshyni657@gmail.com>
2026-06-12 11:19:19 +0530

    added operators.js

---
+++ operators.js
@@ -0,0 +1,9 @@
+//arithmetic
+var a=5
+var b=3
+console.log(a+b) //add
+console.log(a-b) //sub
+console.log(a*b) //mul
+console.log(a/b) //div
+console.log(a%b) //modulus
+console.log(a**b) //exponentiation
