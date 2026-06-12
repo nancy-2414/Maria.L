47b41583a2807d50fa0c079c505c22b4e7174abd
nancy-2414 <nancyshyni657@gmail.com>
2026-06-12 15:08:53 +0530

    added datattypes.js

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+//primitive datatypes
+console.log("numbers");
+var a = 18
+console.log(a);
+
+console.log("string");
+var name = "NancyMaria"
+console.log(name);
+
+console.log("boolean");
+var isstudent = true
+console.log(isstudent);
+
+console.log("null");
+var novalue = null
+console.log(novalue);
+
+console.log("undefined");
+var undefinedvalue
+console.log(undefinedvalue);
+
+//non-primitive datatypes
+console.log("array");
+var fruits = ["apple","orange","pineapple","guava"]; 
+console.log(fruits);
+
+console.log("objects");
+var person =
+{
+      name: "agnes",
+      age: 24
+};
+console.log(person);
