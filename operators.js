fe0c7b2d33b6b299e0ea085c26ba71f983c9e516
nancy-2414 <nancyshyni657@gmail.com>
2026-06-12 11:27:10 +0530

    added operators.js

--- operators.js
+++ operators.js
@@ -1,4 +1,6 @@
 //arithmetic
+console.log("arithmetic operator");
+
 var a=5
 var b=3
 console.log(a+b) //add
@@ -7,3 +9,14 @@ console.log(a*b) //mul
 console.log(a/b) //div
 console.log(a%b) //modulus
 console.log(a**b) //exponentiation
+
+console.log("assignment openrator");
+
+var c =24
+console.log(c+=10);
+console.log(c-=10);
+console.log(c*=10);
+console.log(c/=10);
+
+
+
