98528a01deddf8828b5aa99d6f50eb860411f2bd
nancy-2414 <nancyshyni657@gmail.com>
2026-06-12 15:41:04 +0530

    added functions .js

---
+++ functions.js
@@ -0,0 +1,14 @@
+//non-parametric function
+function greet()
+{
+console.log("hello,world!")
+}
+greet()
+
+
+//parametric functions
+function add(a,b)
+{
+      return a+b
+}
+console.log(add(5,3));
